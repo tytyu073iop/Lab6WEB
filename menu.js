8fe7454517bb4231879d3c7c2e82c15faa5293c9
tytyu073iop <fpm.biryuk@bsu.by>
2025-03-28 19:33:52 +0300

    corrected task

--- menu.js
+++ menu.js
@@ -52,16 +52,89 @@ let menuElements = [
     }
 ]
 
-makeItems(menu, menuElements);
+let horizont = false;
+
+ShowMenu(menuElements, menu);
+
+function ShowMenu(MenuItemsA,ParentElem) {
+    console.log(ParentElem.children);
+    console.log(ParentElem);
+    let cur = ParentElem;
+    while(cur.previousElementSibling) {
+        cur = cur.previousElementSibling;
+        if (cur.firstElementChild) {
+            cur.firstElementChild.remove();
+        }
+    }
+    cur = ParentElem
+    while(cur.nextElementSibling) {
+        cur = cur.nextElementSibling;
+        if (cur.firstElementChild) {
+            cur.firstElementChild.remove();
+        }
+    }
+
+    let horizontal = false;
+    if (ParentElem === menu) {
+        horizontal = true;
+        
+    }
+
+    const rect = ParentElem.getBoundingClientRect();
+    
+    let div = document.createElement("div");
+    div.style.display = "flex";
+
+    ParentElem.appendChild(div);
+    if (!horizontal) {
+        div.style.flexDirection = "column";
+        div.style.position = "fixed";
+        console.log(ParentElem);
+        // const style = window.getComputedStyle(ParentElem);
+        console.log(ParentElem.classList);
+        if(!ParentElem.hor) {
+            console.log("!hor");
+            console.log(ParentElem);
+            console.log(ParentElem.style.flexDirection);
+            div.style.left = `${rect.right}px`;
+            div.style.top = `${rect.top}px`;
+        }
+    } else {
+        div.hor = true;
+    }
+    
+    for (let element of MenuItemsA) {
+        if (element.url) {
+        let a = document.createElement("a");
+        a.href = element.url;
+        a.innerText = element.name;
+        a.classList.add("menuElement");
+        
+        div.appendChild(a);
+        } else if (element.submenu) {
+            let di = document.createElement("div");
+            di.innerText = element.name;
+            di.classList.add("menuElement");
+            di.addEventListener("click", (event) => {event.stopPropagation(); ShowMenu(element.submenu, event.currentTarget);});
+            if (horizontal) {
+                di.hor = true;
+            }
+            div.appendChild(di);
+        }
+    }
+
+    // results;
+    horizont = horizontal;
+}
 
 function makeItems(navElement, menuElements) {
     let ul = document.createElement("ul");
     navElement.appendChild(ul);
-
+    
     for (element of menuElements) {
         let li = document.createElement("li");
         ul.appendChild(li);
-
+        
         if (element.submenu) {
             li.innerHTML = `<span>${element.name}: </span>`;
             makeItems(li, element.submenu);
